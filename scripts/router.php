4251099198498e4ce91a55b4ced41bb7b4bac33b
Christer Edvartsen <cogo@starzinger.net>
2013-01-07 17:57:19 +0100

    Added copyright notice

--- scripts/router.php
+++ scripts/router.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the PHP BitTorrent
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
 if ($_SERVER['SCRIPT_NAME'] === '/announce') {
     require 'public/tracker.php';
     exit;
