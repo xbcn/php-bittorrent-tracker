ed639620f95f07b421a8afc23c65a6065913bb04
Christer Edvartsen <cogo@starzinger.net>
2013-01-08 15:34:12 +0100

    Added version component

---
+++ PHP/BitTorrent/Tracker/Version.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the PHP BitTorrent
+ *
+ * (c) Christer Edvartsen <cogo@starzinger.net>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was
+ * distributed with this source code.
+ */
+
+namespace PHP\BitTorrent\Tracker;
+
+/**
+ * Version class
+ *
+ * @package Tracker
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ * @codeCoverageIgnore
+ */
+class Version {
+    /**
+     * The current version
+     *
+     * @var string
+     */
+    const VERSION = 'dev';
+}
